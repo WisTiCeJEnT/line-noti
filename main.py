71a8e8c7e033bd7394b0b75693cf5f225921bb24
Wattanai Sathuphan <wattanai.s@kbtg.tech>
2021-02-18 02:30:47 +0700

    Using local env var
    commit your token to git is to risky

--- main.py
+++ main.py
@@ -5,11 +5,13 @@ import hashlib
 import hmac
 import json
 import requests
+import os
 
-API_HOST = 'https://api.bitkub.com'
-API_KEY = '8a0b8f4649d9c8c4858181ee286c6714'
-API_SECRET = b'c4e815924288b34c32d942a4cb29a624'
-
+API_HOST = os.getenv('API_HOST')
+API_KEY = os.getenv('API_KEY')
+API_SECRET = bytes(os.getenv('API_SECRET'), 'utf-8')
+url = os.getenv('line_url')
+token = os.getenv('line_token')
 mycoins = ['THB_ADA']
 
 def checkPrice():
@@ -26,8 +28,6 @@ def lineNotify(message):
     return _lineNotify(payload)
 
 def _lineNotify(payload, file=None):
-    url = 'https://notify-api.line.me/api/notify'
-    token = 'ceE0ixKWTLi74RRj8mDgEIMkv6wIVXJd2QngytIruJU'
     headers = {'Authorization' : 'Bearer ' + token}
     return requests.post(url, headers=headers, data = payload, files=file)
 
